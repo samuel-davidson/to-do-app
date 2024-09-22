fe2d089665e4d2adaca8829f00f781e6dc892b37
samuel-davidson <samuelhdavidsonn@gmail.com>
2024-09-22 15:03:38 -0700

    commit 4 - basic layout update

--- to-do-app/main.py
+++ to-do-app/main.py
@@ -4,9 +4,9 @@ def main():
     print_hi(name)
     enter_to_do()
     to_do_item = input()
-    List = ToDo()
-    List.add_task(to_do_item)
-    print(List)
+    to_do_list = ToDo()
+    to_do_list.add_task(to_do_item)
+    print(to_do_list)
 
 
 def print_hi(name):
@@ -20,16 +20,18 @@ def first_message():
 def enter_to_do():
     return print("Please enter a task:")
 
+
 class ToDo:
     def __init__(self):
         self._tasks = []
 
     def add_task(self, task):
         self._tasks.append(task)
-        return (print(f'You have added {task} to your To-Do list'))
+        return print(f'You have added {task} to your To-Do list')
 
     def remove_task(self, task):
         self._tasks.remove(task)
+
 
 if __name__ == "__main__":
     main()
